8cd33f1e087669bb037450c388eb1073677f4233
zigmoon <zigmoon@gmail.com>
2024-08-03 14:12:18 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,13 @@
 import type { Site, SocialObjects } from "./types";
 
 export const SITE: Site = {
-  website: "https://astro-paper.pages.dev/", // replace this with your deployed domain
+  website: "https://astro-zigmoon.vercel.app/", // replace this with your deployed domain
   author: "Sat Naing",
-  desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  desc: "astro news of design",
+  title: "astro zigmoon",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 9,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
